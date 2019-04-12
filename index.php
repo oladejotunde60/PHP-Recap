d8f8da1bbbfbe144ee603b8cd7765247f0251dca
Oladejo Tunde <oladejotunde60@gmail.com>
2019-04-12 04:57:35 +0100

    OOP Concepts

--- index.php
+++ index.php
@@ -1,16 +1,27 @@
+
+<?php
+    $var = "PHP Tutorial for Beginners";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title><?php echo $var; ?> Techbit</title>
 </head>
 <body>
+<h3><?php echo $var; ?></h3>
+<p>PHP is an acronym for Hypertext Preprocessor</p>
+<p><a href="techbit.com.ng">Techbit</a></p>
         <?php
-        /*phpinfo();*/
+        /* To print out PHP version: phpinfo();*/
         echo "Tomorrow I'll learn PHP global variables \n<br>";
         print "This is a bad command : del c:\\*.*";
+
+        cpd@cbn.gov.ng
+        
+    
 
         ?>
 </body>
